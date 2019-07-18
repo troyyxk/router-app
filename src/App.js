8fa0cb509f275b041cedc1d65e11d8a823417fac
Xingkun Yin <xingkun.yin@ceridian.com>
2019-07-18 13:50:38 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import NavBar from "./components/navbar";
+import Products from "./components/products";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Dashboard from "./components/admin/dashboard";
+import ProductDetails from "./components/productDetails";
+import NotFound from "./components/notFound";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="content">
+          <Route path="/products" component={Products} />
+          <Route path="/posts" component={Posts} />
+          <Route path="/admin" component={Dashboard} />
+          <Route path="/" component={Home} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
